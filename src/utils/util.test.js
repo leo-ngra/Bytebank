3911c2446cc02473986f0d39f159e0899c3a28de
leo-ngra <leonardo.ngra@gmail.com>
2023-01-09 09:39:18 -0300

    Realizando testes no deposito e transferencia

---
+++ src/utils/util.test.js
@@ -0,0 +1,36 @@
+import { calculaNovoSaldo } from './index'
+
+describe('Quando eu realizo uma transacao', () => {
+    test('Que é um depósito, o saldo deve aumentar', () => {
+        const transacao = {
+            transacao: 'Depósito',
+            valor: 50
+        }
+        const novoSaldo = calculaNovoSaldo(transacao, 100)
+
+        expect(novoSaldo).toBe(150)
+    })
+
+    test('Que é uma transfência, o saldo deve diminuir', () => {
+        const transacao = {
+            transacao: 'Transferência',
+            valor: 50
+        }
+        const novoSaldo = calculaNovoSaldo(transacao, 100)
+
+        expect(novoSaldo).toBe(50)
+    })
+})
+
+test('Deve retornar o valor do saldo atualizado com o rendimento', () => {
+    const calculaRendimento = jest.fn(saldo => saldo + saldo*0.005)
+
+    const saldo = 100
+
+    const novoSaldo = calculaRendimento(saldo)
+
+    expect(novoSaldo).toBe(100.5)
+    expect(calculaRendimento).toHaveBeenCalled()
+    expect(calculaRendimento).toHaveBeenCalledWith(saldo)
+})
+
